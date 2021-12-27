8b9b0c2e0effcb648aa82149bf87b552f8f8f791
HydraITlx <82394926+HydraITlx@users.noreply.github.com>
2021-12-27 12:43:20 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div>
+      <h1>React App</h1>
+      <h2>{new Date().toDateString()}</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos
+        delectus voluptatem ab repellat saepe eos molestiae cupiditate, omnis
+        doloremque, necessitatibus totam consectetur porro sint quam libero
+        aperiam? Laborum, quaerat?
+      </p>
+    </div>
+  );
+}
